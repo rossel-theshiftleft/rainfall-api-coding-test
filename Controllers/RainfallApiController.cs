253dbdd00020ad615c2d6d3ce86e89c66e78643d
Rossel <rossel@theshiftleft.com>
2024-01-22 16:26:05 +0800

    http get enpoint for id/{stationId}/readings

--- Controllers/RainfallApiController.cs
+++ Controllers/RainfallApiController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 
 namespace RainfallApi.Controllers
 {
@@ -7,5 +9,35 @@ namespace RainfallApi.Controllers
     [ApiController]
     public class RainfallApiController : ControllerBase
     {
+        readonly string apiURL = "https://environment.data.gov.uk/flood-monitoring/";
+
+        // GET api/<RainfallApiController>/id/3901/readings
+        [HttpGet("id/{stationId}/readings")]
+        public async Task<IActionResult> Get(
+            [Description("The id of the reading station")] string stationId,
+            [FromQuery][Range(1, 100)][Description("The number of readings to return\r\n")] int? count = 10
+         )
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage response = await httpClient.GetAsync($"{apiURL}id/stations/{stationId}/readings?_limit={count}");
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        // Read and parse the JSON response
+                        var responseData = await response.Content.ReadAsStringAsync();
+
+                        return Ok(responseData);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error" + ex.Message);
+                }
+            }
+            return Ok();
+        }
     }
 }
